d6598d3049730ca5de1ef26e9253e0ab7eddc931
Rehann87 <155806843+Rehann87@users.noreply.github.com>
2024-10-05 18:28:46 +0530

    Update routes.category.js

--- src/category/routes.category.js
+++ src/category/routes.category.js
@@ -2,7 +2,8 @@ const router = require("express").Router()
 const categoryController = require("./controller.category")
 
 router.post("/add-category", categoryController.addCategory);
-router.get("/get-category", categoryController.getAllCategories)
+router.get("/get-category", categoryController.getAllCategories);
+router.get("/get-singlecategory/:Id", categoryController.getSingleCategory);
 router.patch("/update-category/:categoryId", categoryController.updateCategory);
 router.delete("/delete-category/:categoryId", categoryController.deleteCategory);
 
