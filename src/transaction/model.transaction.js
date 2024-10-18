25ecce0672dff0875f198ede3ff5b2aab14bf878
Rehan sheikh <155806843+Rehann87@users.noreply.github.com>
2024-10-18 18:46:14 +0530

    Update model.transaction.js

--- src/transaction/model.transaction.js
+++ src/transaction/model.transaction.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
 
 const transactionSchema = new mongoose.Schema({
+    categoryId:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Category",
+        required: true,
+    },
     amount: {
         type: Number,
         required: true
