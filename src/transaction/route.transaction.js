1120d3d86e308b28d7ee410a5f7d13b7c26ce341
Rehan sheikh <155806843+Rehann87@users.noreply.github.com>
2024-10-18 13:13:59 +0530

    Update route.transaction.js

--- src/transaction/route.transaction.js
+++ src/transaction/route.transaction.js
@@ -3,6 +3,7 @@ const transactionController = require("./controller.transaction")
 
 router.post("/add-transaction", transactionController.addTransaction);
 router.get("/get-transaction", transactionController.getAllTransaction);
+router.get("/get-single-transaction/:transactionId", transactionController.getSingleTransaction);
 router.patch("/update-transaction/:transactionId", transactionController.updatedTransaction);
 router.delete("/delete-transaction/:transactionId", transactionController.deletedTransaction)
 
